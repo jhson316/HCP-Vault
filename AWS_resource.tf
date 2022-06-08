d18760db3786029af55b77973c6e23acc3db6d09
jhson316 <fkwnaak@gmail.com>
2022-06-08 05:09:50 +0000

    Plan3

--- AWS_resource.tf
+++ AWS_resource.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.17.1"
-    }
-  }
-}
-
 # provider "aws" {
 #   # Configuration options
 # }
--- Vault-resource.tf
+++ Vault-resource.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    hcp = {
-      source = "hashicorp/hcp"
-      version = "0.30.0"
-    }
-  }
-}
-
 provider "hcp" {
   # Configuration options
 }
