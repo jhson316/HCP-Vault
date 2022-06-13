f427eb04eef4a8c21644ea9c28bb3a195abc0ee6
jhson316 <89453346+jhson316@users.noreply.github.com>
2022-06-13 15:39:52 +0900

    output TEST4

--- AWS_output.tf
+++ AWS_output.tf
@@ -17,7 +17,7 @@ output "vpc_id" {
 }
 
 output "SSH" {
-  value = "ssh -i \"TEST.pem\" ec2-user@"
+  value = format("%s-%s", "ssh -i \"TEST.pem\" ec2-user@", aws_instance.app_server.public_dns)
   # value = "ssh -i \"TEST.pem\" ec2-user@" && aws_instance.app_server.public_dns
   # value = aws_instance.app_server.public_dns
 }
