60f29cbe5fb393c0cfde5ab6381b8eae391e4c2a
jhson316 <fkwnaak@gmail.com>
2022-06-13 08:16:10 +0000

    VPC Test4

--- AWS_output.tf
+++ AWS_output.tf
@@ -4,13 +4,20 @@ output "SSH" {
   value = format("%s%s", "ssh -i \"TEST.pem\" ec2-user@", aws_instance.app_server.public_dns)
 }
 
-output "vpc_arn" {
-  value = aws_vpc.peer.arn
-}
-
 output "account_id" {
   value = data.aws_caller_identity.current.account_id
 }
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_default_vpc.default_vpc  
+}
+output "Default_igw_id" {
+  value = aws_internet_gateway.default.id #aws_default_vpc.default_vpc.default_route_table_id
+}
+
+# output "vpc_arn" {
+#   value = aws_vpc.peer.arn
+# }
 
 # output "caller_arn" {
 #   value = data.aws_caller_identity.current.arn
@@ -20,9 +27,9 @@ output "account_id" {
 #   value = data.aws_caller_identity.current.user_id
 # }
 
-output "vpc_id" {
-  value = aws_default_vpc.default_vpc.id
-}
+# output "vpc_id" {
+#   value = aws_default_vpc.default_vpc.id
+# }
 
 # output "security_group_id" {
 #   value = aws_default_security_group.default.id
